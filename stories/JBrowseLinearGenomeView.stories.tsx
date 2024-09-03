3e61f96c361bfd1fc5f6e8c3119657c8d54f823b
Colin <colin.diesh@gmail.com>
2024-09-03 13:09:20 -0400

    Still

--- stories/JBrowseLinearGenomeView.stories.tsx
+++ stories/JBrowseLinearGenomeView.stories.tsx
@@ -1,24 +1,3 @@
-export {
-  DefaultSession,
-  DisableAddTrack,
-  HumanExomeExample,
-  NextstrainExample,
-  OneLinearGenomeView,
-  UsingLocObject,
-  WithAggregateTextSearching,
-  WithCustomTheme,
-  WithExternalPlugin,
-  WithInlinePlugins,
-  WithInternetAccounts,
-  WithObserveVisibleRegions,
-  WithObserveVisibleFeatures,
-  WithOutsideStyling,
-  WithPerTrackTextSearching,
-  WithShowTrack,
-  WithWebWorker,
-  WithTwoLinearGenomeViews,
-  WithReact18,
-  WithErrorHandler,
-} from './examples'
+export { OneLinearGenomeView } from './examples'
 
 export default { title: 'Source code for examples' }
--- stories/examples/OneLinearGenomeView.tsx
+++ stories/examples/OneLinearGenomeView.tsx
@@ -1,28 +1,88 @@
-/* eslint-disable no-console */
 import React from 'react'
-
-// in your code:
-// import {createViewState, JBrowseLinearGenomeView} from '@jbrowse/react-linear-genome-view'
 import { createViewState, JBrowseLinearGenomeView } from '../../src'
-import { getVolvoxConfig } from './util'
 
 export const OneLinearGenomeView = () => {
-  const { assembly, tracks } = getVolvoxConfig()
   const state = createViewState({
-    assembly,
-    tracks,
-    // use 1-based coordinates for locstring
-    location: 'ctgA:1105..1221',
-    onChange: patch => {
-      console.log('patch', patch)
+    assembly: {
+      name: 'GRCh38',
+      aliases: ['hg38'],
+      sequence: {
+        type: 'ReferenceSequenceTrack',
+        trackId: 'GRCh38-ReferenceSequenceTrack',
+        adapter: {
+          type: 'BgzipFastaAdapter',
+          fastaLocation: {
+            uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz',
+          },
+          faiLocation: {
+            uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.fai',
+          },
+          gziLocation: {
+            uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.gzi',
+          },
+        },
+      },
+      refNameAliases: {
+        adapter: {
+          type: 'RefNameAliasAdapter',
+          location: {
+            uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
+          },
+        },
+      },
     },
+    tracks: [
+      {
+        type: 'FeatureTrack',
+        trackId: 'genes',
+        name: 'NCBI RefSeq Genes',
+        assemblyNames: ['GRCh38'],
+        category: ['Genes'],
+        adapter: {
+          type: 'Gff3TabixAdapter',
+          gffGzLocation: {
+            uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
+          },
+          index: {
+            location: {
+              uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.tbi',
+            },
+          },
+        },
+        textSearching: {
+          textSearchAdapter: {
+            type: 'TrixTextSearchAdapter',
+            textSearchAdapterId: 'gff3tabix_genes-index',
+            ixFilePath: {
+              uri: 'https://jbrowse.org/genomes/GRCh38/ncbi_refseq/trix/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.ix',
+            },
+            ixxFilePath: {
+              uri: 'https://jbrowse.org/genomes/GRCh38/ncbi_refseq/trix/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.ixx',
+            },
+            metaFilePath: {
+              uri: 'https://jbrowse.org/genomes/GRCh38/ncbi_refseq/trix/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz_meta.json',
+            },
+            assemblyNames: ['GRCh38'],
+          },
+        },
+      },
+      {
+        type: 'FeatureTrack',
+        trackId: 'repeats_hg38',
+        name: 'Repeats',
+        assemblyNames: ['hg38'],
+        category: ['Annotation'],
+        adapter: {
+          type: 'BigBedAdapter',
+          bigBedLocation: {
+            uri: 'https://jbrowse.org/genomes/GRCh38/repeats.bb',
+            locationType: 'UriLocation',
+          },
+        },
+      },
+    ],
+    // use 1-based coordinates for locstring
+    location: 'chr1:1-100,000,000',
   })
-  return (
-    <div>
-      <JBrowseLinearGenomeView viewState={state} />
-      <a href="https://github.com/GMOD/jbrowse-components/blob/main/products/jbrowse-react-linear-genome-view/stories/examples/OneLinearGenomeView.tsx">
-        Source code
-      </a>
-    </div>
-  )
+  return <JBrowseLinearGenomeView viewState={state} />
 }
